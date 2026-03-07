c8c060496c9a2c87fcd7258981951b9ff5c8999c
Manu Shukla <manushukla64@gmail.com>
2026-03-07 18:38:07 +0530

    Initial commit - 30 days python learning

---
+++ Variable _data_type_day_1.py
@@ -0,0 +1,19 @@
+from loguru import logger
+_1st_labour = "Mahesh"
+_2nd_labour = "Mithlesh"
+_3rd_labour = "ramesh"
+_4th_labour = "sumesh"
+
+_1st_labour_wage = 500
+_2nd_labour_wage =  400
+_3rd_labour_wage = 400
+_4th_labour_wage = 300
+
+
+from loguru import logger
+logger.info(f"\"\"\" Programming aasan hai. We are going to learn this in depth. While learning we have to make sure that \n we are implementing all the logics by ourself. "
+            f"The aim here is to build our \"4 BHK\" house with the \n help of 'Python programming'. We have total land is of \\\100 ft * 100ft /, to complete the house \n we have total 6 labours with 'different skill set like \"\\\ building wall or building roof \\\ \". \n \t I have to print this paragraph as it is given here.\"\"\"")
+
+print(id(_1st_labour),id(_2nd_labour),id(_3rd_labour),id(_4th_labour))
+
+print(id(_1st_labour_wage),id(_2nd_labour_wage),id(_3rd_labour_wage),id(_4th_labour_wage))
